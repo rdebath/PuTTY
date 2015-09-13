d04cc06b8f511530134b7b3490cc02e5b0660847
Robert de Bath <rdebath@tvisiontech.co.uk>
2015-09-13 11:59:56 +0100

    UnixGTK: Fix big cursor CSI sequence.
    This enables the sequence "CSI 34 l" and "CSI 34 h" to allow the host
    to switch the cursor to the big block (if it's currently the one of the
    line cursors)
    
    Windows already worked like this.

--- unix/gtkwin.c
+++ unix/gtkwin.c
@@ -3412,13 +3412,14 @@ void do_cursor(Context ctx, int x, int y, wchar_t *text, int len,
     struct gui_data *inst = dctx->inst;
 
     int active, passive, widefactor;
+    int cur_type = inst->term->big_cursor?0:inst->cursor_type;
 
     if (attr & TATTR_PASCURS) {
 	attr &= ~TATTR_PASCURS;
 	passive = 1;
     } else
 	passive = 0;
-    if ((attr & TATTR_ACTCURS) && inst->cursor_type != 0) {
+    if ((attr & TATTR_ACTCURS) && cur_type != 0) {
 	attr &= ~TATTR_ACTCURS;
         active = 1;
     } else
@@ -3443,7 +3444,7 @@ void do_cursor(Context ctx, int x, int y, wchar_t *text, int len,
 	len *= 2;
     }
 
-    if (inst->cursor_type == 0) {
+    if (cur_type == 0) {
 	/*
 	 * An active block cursor will already have been done by
 	 * the above do_text call, so we only need to do anything
@@ -3468,7 +3469,7 @@ void do_cursor(Context ctx, int x, int y, wchar_t *text, int len,
 	else
 	    char_width = inst->font_width;
 
-	if (inst->cursor_type == 1) {
+	if (cur_type == 1) {
 	    uheight = inst->fonts[0]->ascent + 1;
 	    if (uheight >= inst->font_height)
 		uheight = inst->font_height - 1;
