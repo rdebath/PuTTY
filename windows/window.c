836b339081b8598fdc4b814ce14f0b4b7d816fae
Robert de Bath <rdebath@tvisiontech.co.uk>
2015-10-13 09:26:10 +0100

    Change Ctrl-Backspace to match XTerm (Windows)
    Shift-Backspace sends the alternative Backspace code, this adds
    Ctrl-Backspace to do the same.
    
    The xterm variants seem to prefer to use this and so do my fingers.
    (For the first time I've had to use this in years)

--- windows/window.c
+++ windows/window.c
@@ -4381,14 +4381,9 @@ static int TranslateKey(UINT message, WPARAM wParam, LPARAM lParam,
 	    }
 	}
 
-	if (wParam == VK_BACK && shift_state == 0) {	/* Backspace */
-	    *p++ = (conf_get_int(conf, CONF_bksp_is_delete) ? 0x7F : 0x08);
-	    *p++ = 0;
-	    return -2;
-	}
-	if (wParam == VK_BACK && shift_state == 1) {	/* Shift Backspace */
-	    /* We do the opposite of what is configured */
-	    *p++ = (conf_get_int(conf, CONF_bksp_is_delete) ? 0x08 : 0x7F);
+	if (wParam == VK_BACK && (shift_state&-3) == 0) {	/* Backspace */
+	    *p++ = (!conf_get_int(conf, CONF_bksp_is_delete) != !shift_state
+		    ? 0x7F : 0x08);
 	    *p++ = 0;
 	    return -2;
 	}
