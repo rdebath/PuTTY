94e5e923efdf11a5100bdd570eb0532619695a52
Robert de Bath <rdebath@tvisiontech.co.uk>
2015-11-21 11:34:29 +0000

    BUGFIX: OSC P XYZ should be eaten.
    Despite initially looking like a Linux palette setting sequence.

--- terminal.c
+++ terminal.c
@@ -4522,6 +4522,7 @@ static void term_out(Terminal *term)
 		switch (c) {
 		  case 'P':	       /* Linux palette sequence */
 		    term->termstate = SEEN_OSC_P;
+		    term->esc_args[0] = UINT_MAX;
 		    term->osc_strlen = 0;
 		    break;
 		  case 'R':	       /* Linux palette reset */
@@ -4618,7 +4619,7 @@ static void term_out(Terminal *term)
 		    else if ((int)c >= 'a' && (int)c <= 'a' + max - 10)
 			val = c - 'a' + 10;
 		    else {
-			term->termstate = TOPLEVEL;
+			term->termstate = OSC_STRING;
 			break;
 		    }
 		    term->osc_string[term->osc_strlen++] = val;
